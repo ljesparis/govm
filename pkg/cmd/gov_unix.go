934905e867bc0b4186c2f2dff71780ed91ad8122
Leonardo Javier Esparis Meza <ljesparis@gmail.com>
2020-03-11 08:55:09 +0100

    Added build line for unix systems

---
+++ pkg/cmd/gov_unix.go
@@ -0,0 +1,11 @@
+// +build aix darwin dragonfly freebsd linux  netbsd openbsd solaris
+package cmd
+
+
+const (
+	defaultGoBinDir = "/usr/local/bin"
+
+	goSourceBin = "go/bin/go"
+	gofmtSourceBin = "go/bin/gofmt"
+)
+
