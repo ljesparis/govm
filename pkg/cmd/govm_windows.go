4744a5741fd6e2edc14731d3a0eb1ade3d66f7fe
Leonardo Esparis <ljesparis@gmail.com>
2020-04-04 22:26:45 +0200

    Added bindir function
    Function that returns the binary directory for windows

--- pkg/cmd/govm_windows.go
+++ pkg/cmd/govm_windows.go
@@ -1,11 +1,16 @@
 package cmd
 
-const (
-	defaultGoBinDir = ""
+import (
+	"path"
+)
 
+const (
 	goBin = "go.exe"
 	gofmtBin = "gofmt.exe"
 	goSourceBin    = "go\\bin\\go.exe"
 	gofmtSourceBin = "go\\bin\\gofmt.exe"
 )
 
+func binDir(home string) string {
+	return path.Join(home, "bin")
+}
